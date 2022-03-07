e0541c08133c5f753eb046db7b6486c8b5a2b8e2
corourke <cameron.orourke@gmail.com>
2022-03-06 19:18:00 -0800

    Viewport fix, Scale setting, transp open/closed

--- README.md
+++ README.md
@@ -1,28 +1,9 @@
-## Incorta Candlestick chart
+## Incorta Candlestick Chart Component
 
-Displays a candlestick chart. This is an example
-of combinining real-time data fetched from an external source with stored data. This
-technique could also be used in FOREX appliciations.
+Displays a candlestick chart.
 
-<img width="341" alt="Sample of rendered crypto tile" src="assets/sample.png">
-
-To try it out:
-
-(These are abbreviated instructions -- for a more complete guide, see [Getting Started with the Incorta Component SDK](https://community.incorta.com/t/p8hjygp/getting-started-component-sdk)
-
-- Install the create-incorta-component scripts with `% npm install -g @incorta-org/create-incorta-component`
-- `git clone` the repository
-- `cd` into the project directory and install dependencies: `npm install`
-- Run the incorta component server: `create-incorta-component start`
-- Upload the `data/historical.csv` file into Incorta and create a schema for it. Be sure to load the data.
-- Navigate to the Incorta analyzer and choose the Developer Visual plugin
-- Drag columns containing coin identifiers (i.e. 'bitcoin', 'ethereum'), and column containing coin holdings (quantity) into the binding tray
-
-See the [SETUP](./SETUP.md) doc for development environment setup instructions.
+<img alt="Sample of rendered crypto tile" src="assets/sample.png">
 
 #### Future improvements
 
-- Internationalization
-- Allow the user to select a base currency
-- Computation of total portfolio value
-- Add tests
+- Ability to add a moving average line or bollinger bands.
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React, { useRef } from 'react';
 import ReactECharts from 'echarts-for-react';
 import { ComponentProps } from '@incorta-org/component-sdk';
 
-import { useFullViewport } from 'hooks';
-import { BASE_CHART_OPTIONS } from 'constants/general';
+// import { useFullViewport } from 'hooks';
+// import { BASE_CHART_OPTIONS } from 'constants/general';
 
+// This is the order of the trays, see definition.json
 const TIMESTAMP_INDEX = 0;
 const CLOSE_INDEX = 1;
 const OPEN_INDEX = 2;
@@ -15,29 +16,62 @@ const CandlestickChart = (props: ComponentProps) => {
   const ref = useRef(null);
   const data = props.response.data;
 
-  const {
-    rect: { width, height }
-  } = useFullViewport(ref);
+  const { width, height } = props.context.component.dimensions;
 
   const dateData = data.map(cell => cell[TIMESTAMP_INDEX]?.formatted);
 
+  // The eCharts candlestick chart expects the data in this order:
+  // [open, close, lowest, highest]
+  // https://echarts.apache.org/en/option.html#series-candlestick.data
   const seriesData = data.map(cell => [
-    Number(cell[CLOSE_INDEX]?.value),
     Number(cell[OPEN_INDEX]?.value),
+    Number(cell[CLOSE_INDEX]?.value),
     Number(cell[LOW_INDEX]?.value),
     Number(cell[HIGH_INDEX]?.value)
   ]);
 
   const options = {
-    ...BASE_CHART_OPTIONS,
     xAxis: {
       data: dateData
     },
-    yAxis: {},
+    yAxis: {
+      scale: props.context.component.settings?.scale && true
+    },
     series: [
       {
         type: 'candlestick',
         data: seriesData
+      }
+    ],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross'
+      },
+      borderWidth: 1,
+      borderColor: '#ccc',
+      padding: 10,
+      textStyle: {
+        color: '#000'
+      }
+    },
+    axisPointer: {
+      link: [
+        {
+          xAxisIndex: 'all'
+        }
+      ],
+      label: {
+        backgroundColor: '#777'
+      }
+    },
+    dataZoom: [
+      {
+        type: 'slider',
+        xAxisIndex: [0, 1],
+        filterMode: 'filter',
+        start: 0,
+        end: 100
       }
     ]
   };
